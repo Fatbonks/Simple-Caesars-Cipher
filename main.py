5910b368f670ba9db70a1ba296cc4684ad241e3e
Fatbonks <106663968+Fatbonks@users.noreply.github.com>
2022-11-15 07:24:27 +1300

    V5 Added the encrypt function and decrypt function, finish routine

--- main.py
+++ main.py
@@ -24,11 +24,72 @@ def get_key(shift):
             print("Please only enter numbers")
 
 
+# Encrypts words using this formula (letter = (letter + key) % 26 #
+def encrypt_word(word, shift, encrypted_list, encoded_word):
+    value = 0
+    for element in word:
+        for i in range(len(string.ascii_letters) - 1):
+            if element == string.ascii_letters[i]:
+                encrypted_word = (i + shift) % len(string.ascii_letters)
+                encrypted_list.insert(value, string.ascii_letters[encrypted_word])
+                value += 1
+        if element == " ":
+            encrypted_list.insert(value, element)
+            value += 1
+        for punc in range(len(string.punctuation)):
+            if element == string.punctuation[punc]:
+                encrypted_list.insert(value, element)
+                value += 1
+    for i in range(len(encrypted_list)):
+        encoded_word = encoded_word + encrypted_list[i]
+    return encoded_word
+
+
+# Decrypts words using this formula (letter = (letter - key) % 26 #
+def decrypt_word(word, shift, decrypted_list, decoded_word):
+    value = 0
+    for element in word:
+        for i in range(len(string.ascii_letters) - 1):
+            if element == string.ascii_letters[i]:
+                encrypted_word = (i - shift) % len(string.ascii_letters)
+                decrypted_list.insert(value, string.ascii_letters[encrypted_word])
+                value += 1
+        if element == " ":
+            decrypted_list.insert(value, element)
+            value += 1
+        for punc in range(len(string.punctuation)):
+            if element == string.punctuation[punc]:
+                decrypted_list.insert(value, element)
+                value += 1
+    for letter in range(len(decrypted_list)):
+        decoded_word = decoded_word + decrypted_list[letter]
+    return decoded_word
+
+
+# variables
 user_input = ''
 key = 0
+encrypt_list = []
+decrypt_list = []
+new_encoded_word = ""
+new_decoded_word = ""
+# main routine #
 while True:
-    user_input = get_sentence(user_input)
-    key = get_key(key)
-    print(user_input)
-    print(key)
-    break
+    # Forces valid input #
+    while True:
+        try:
+            ans = int(input("1:Encrypt\n2:Decrypt\nAnswer here:"))
+            break
+        except ValueError:
+            print("please only use numbers")
+    if ans == 1:
+        user_input = get_sentence(user_input)
+        key = get_key(key)
+        new_encoded_word = encrypt_word(user_input, key, encrypt_list, new_encoded_word)
+        print(new_encoded_word)
+    elif ans == 2:
+        user_input = get_sentence(user_input)
+        key = get_key(key)
+        new_decoded_word = decrypt_word(user_input, key, decrypt_list, new_decoded_word)
+        print(new_decoded_word)
+    input("press enter to continue")
