35c1213489c73d4dd7e727b5f6bfd04ed5a7e946
Fatbonks <106663968+Fatbonks@users.noreply.github.com>
2022-11-20 19:21:57 +1300

    V6 Fixed some bugs in the code, can now also use other simple caesars cipher with mine

--- main.py
+++ main.py
@@ -7,8 +7,8 @@ def get_sentence(user):
     # Forces the user to only input sentences that don't have numbers #
     while True:
         user = input("Please enter your sentence below\n:")
-        if re.search(r'[0-9]', user_input):
-            print("Please input your sentence again without numbers,\nSpaces and punctuation will work")
+        if re.search(r'\d', user):
+            print("Please input your sentence again without numbers")
         else:
             return user
 
@@ -27,42 +27,73 @@ def get_key(shift):
 # Encrypts words using this formula (letter = (letter + key) % 26 #
 def encrypt_word(word, shift, encrypted_list, encoded_word):
     value = 0
-    for element in word:
-        for i in range(len(string.ascii_letters) - 1):
-            if element == string.ascii_letters[i]:
-                encrypted_word = (i + shift) % len(string.ascii_letters)
-                encrypted_list.insert(value, string.ascii_letters[encrypted_word])
+    # Gets each Letter in the word that user has inputted #
+    for letter in word:
+        # gets the length of the alphabet for lowercase #
+        for length_of_alphabet_lowercase in range(len(string.ascii_lowercase)):
+            if letter == string.ascii_lowercase[length_of_alphabet_lowercase]:
+                # Shift is what number the user inputted for their key #
+                encrypted_word = (length_of_alphabet_lowercase + shift) % len(string.ascii_lowercase)
+                # after the word gets encrypted it goes into this list #
+                encrypted_list.insert(value, string.ascii_lowercase[encrypted_word])
+                # value is where the next string that come is going to go that why I add one #
                 value += 1
-        if element == " ":
-            encrypted_list.insert(value, element)
+        # gets the length of the alphabet for uppercase #
+        for length_of_alphabet_uppercase in range(len(string.ascii_uppercase)):
+            if letter == string.ascii_uppercase[length_of_alphabet_uppercase]:
+                # Shift is what number the user inputted for their key #
+                encrypted_word = (length_of_alphabet_uppercase + shift) % len(string.ascii_uppercase)
+                # after the word gets encrypted it goes into this list #
+                encrypted_list.insert(value, string.ascii_uppercase[encrypted_word])
+                # value is where the next string that come is going to go that why I add one #
+                value += 1
+        # checks if the string is a space and then adds it into the list #
+        if letter == " ":
+            encrypted_list.insert(value, letter)
             value += 1
+        # checks if the letter is some sort of punctuation and if it is, it adds the Letter to the list #
         for punc in range(len(string.punctuation)):
-            if element == string.punctuation[punc]:
-                encrypted_list.insert(value, element)
+            if letter == string.punctuation[punc]:
+                encrypted_list.insert(value, letter)
                 value += 1
-    for i in range(len(encrypted_list)):
-        encoded_word = encoded_word + encrypted_list[i]
+    # goes through the list and then adds each letter in the list into a string to then be used in any other decrypter #
+    for encrypted_letter in range(len(encrypted_list)):
+        encoded_word = encoded_word + encrypted_list[encrypted_letter]
     return encoded_word
 
 
 # Decrypts words using this formula (letter = (letter - key) % 26 #
 def decrypt_word(word, shift, decrypted_list, decoded_word):
     value = 0
-    for element in word:
-        for i in range(len(string.ascii_letters) - 1):
-            if element == string.ascii_letters[i]:
-                encrypted_word = (i - shift) % len(string.ascii_letters)
-                decrypted_list.insert(value, string.ascii_letters[encrypted_word])
+    # Gets each Letter in the word that user has inputted #
+    for letter in word:
+        # gets the length of the alphabet for uppercase #
+        for length_of_alphabet_lowercase in range(len(string.ascii_lowercase)):
+            if letter == string.ascii_lowercase[length_of_alphabet_lowercase]:
+                # Shift is what number the user inputted for their key #
+                encrypted_word = (length_of_alphabet_lowercase - shift) % len(string.ascii_lowercase)
+                # after the word gets decrypted it goes into this list #
+                decrypted_list.insert(value, string.ascii_lowercase[encrypted_word])
                 value += 1
-        if element == " ":
-            decrypted_list.insert(value, element)
+
+        for length_of_alphabet_uppercase in range(len(string.ascii_uppercase)):
+            if letter == string.ascii_uppercase[length_of_alphabet_uppercase ]:
+                encrypted_word = (length_of_alphabet_uppercase  - shift) % len(string.ascii_uppercase)
+                decrypted_list.insert(value, string.ascii_uppercase[encrypted_word])
+                # value is where the next string that come is going to go that why I add one #
+                value += 1
+        # checks if the string is a space and then adds it into the list #
+        if letter == " ":
+            decrypted_list.insert(value, letter)
             value += 1
+        # checks if the letter is some sort of punctuation and if it is, it adds the Letter to the list #
         for punc in range(len(string.punctuation)):
-            if element == string.punctuation[punc]:
-                decrypted_list.insert(value, element)
+            if letter == string.punctuation[punc]:
+                decrypted_list.insert(value, letter)
                 value += 1
-    for letter in range(len(decrypted_list)):
-        decoded_word = decoded_word + decrypted_list[letter]
+    # goes through the list and then adds each letter in the list into a string to then be used in any other encrypter #
+    for decrypted_letter in range(len(decrypted_list)):
+        decoded_word = decoded_word + decrypted_list[decrypted_letter]
     return decoded_word
 
 
@@ -75,6 +106,10 @@ new_encoded_word = ""
 new_decoded_word = ""
 # main routine #
 while True:
+    encrypt_list = []
+    decrypt_list = []
+    new_encoded_word = ""
+    new_decoded_word = ""
     # Forces valid input #
     while True:
         try:
@@ -87,9 +122,12 @@ while True:
         key = get_key(key)
         new_encoded_word = encrypt_word(user_input, key, encrypt_list, new_encoded_word)
         print(new_encoded_word)
+        input("press enter to continue")
     elif ans == 2:
         user_input = get_sentence(user_input)
         key = get_key(key)
         new_decoded_word = decrypt_word(user_input, key, decrypt_list, new_decoded_word)
         print(new_decoded_word)
-    input("press enter to continue")
+        input("press enter to continue")
+    elif ans != 1 or ans != 2:
+        print("please input 1 or 2")
